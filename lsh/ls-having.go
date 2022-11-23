a5576eb7d6ffb952b77cc9ac62acd9a03441cc84
James Hu <james.hu.ustc@hotmail.com>
2022-11-24 00:18:03 +1100

    wip: matching logic

--- lsh/ls-having.go
+++ lsh/ls-having.go
@@ -11,49 +11,51 @@ import (
 )
 
 type Options struct {
-	Depth            int
-	Exclude          glob.Glob
-	NoDefaultExclude bool
-	FlagFile         string
-	CheckFile        string
-	CheckRegexp      regexp.Regexp
-	CheckInverse     bool
+	Depth        int
+	Exclude      glob.Glob
+	FlagFile     glob.Glob
+	CheckFile    string // "" means it is not used
+	CheckRegexp  regexp.Regexp
+	CheckInverse bool
 }
 
 func LsHaving(options *Options, dir string) []string {
 	var found []string = make([]string, 0, 100)
-	doLsHaving(options, &found, dir, nil)
+	doLsHaving(options, &found, dir, 0, nil) // root dir has depth 0
 	return found
 }
 
 type dirEntryEx struct {
 	Path  string
+	Depth int
 	Entry fs.DirEntry
 }
 
-func readEntries(dir string) *[]dirEntryEx {
+// Read all entries under the specified directory.
+// The "depth" parameter is the depth of the directory specified by "dir" parameter.
+func readEntries(dir string, depth int) *[]dirEntryEx {
 	entries, err := os.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
 	entriesEx := make([]dirEntryEx, 0, len(entries))
 	for _, entry := range entries {
-		entriesEx = append(entriesEx, dirEntryEx{filepath.Join(dir, entry.Name()), entry})
+		entriesEx = append(entriesEx, dirEntryEx{filepath.Join(dir, entry.Name()), depth + 1, entry})
 	}
 	return &entriesEx
 }
 
-func doLsHaving(options *Options, found *[]string, dir string, entriesInDir *[]dirEntryEx) {
+func doLsHaving(options *Options, found *[]string, dir string, depth int, entriesInDir *[]dirEntryEx) {
 	if entriesInDir == nil {
-		entriesInDir = readEntries(dir)
+		entriesInDir = readEntries(dir, depth)
 	}
 	for _, entry := range *entriesInDir {
 		if shouldCheck(options, &entry) {
-			entriesInSubDir := readEntries(entry.Path)
+			entriesInSubDir := readEntries(entry.Path, entry.Depth)
 			if match(options, &entry, entriesInSubDir) {
 				*found = append(*found, entry.Path)
 			}
-			doLsHaving(options, found, entry.Path, entriesInSubDir)
+			doLsHaving(options, found, entry.Path, entry.Depth, entriesInSubDir)
 		}
 	}
 }
@@ -62,14 +64,29 @@ func shouldCheck(options *Options, dir *dirEntryEx) bool {
 	if !dir.Entry.IsDir() {
 		return false
 	}
+	if options.Depth >= 0 && options.Depth < dir.Depth {
+		return false
+	}
+	if options.Exclude != nil && options.Exclude.Match(dir.Path) {
+		return false
+	}
 	return true
 }
 
 func match(options *Options, dir *dirEntryEx, entries *[]dirEntryEx) bool {
+	foundFlagFile := false
 	for _, entry := range *entries {
-		if entry.Entry.Name() == "serverless.yml" {
-			return true
+		if options.FlagFile.Match(entry.Entry.Name()) {
+			foundFlagFile = true
+			break
 		}
 	}
-	return false
+	var checkFileMismatch bool
+	if options.CheckFile == "" {
+		checkFileMismatch = false
+	} else {
+		// read the file
+		// do regexp match
+	}
+	return foundFlagFile && !checkFileMismatch
 }
