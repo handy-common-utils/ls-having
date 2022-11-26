735ed6e89e922d310767ff85f1cb4001b7e98769
James Hu <james.hu.ustc@hotmail.com>
2022-11-26 21:43:23 +1100

    parse command line arguments

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/handy-common-utils/ls-having
 go 1.19
 
 require github.com/gobwas/glob v0.2.3
+
+require github.com/josephvusich/go-getopt v1.0.0
--- main.go
+++ main.go
@@ -1,28 +1,78 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/gobwas/glob"
 	"github.com/handy-common-utils/ls-having/lsh"
+	"github.com/josephvusich/go-getopt"
 )
 
 func main() {
-	var dirs = lsh.LsHaving(&lsh.Options{
-		Depth: 10,
-		Excludes: compileGlobs([]string{
+	var optHelp = flag.Bool("help", false, "show help information")
+	var optDepth = flag.Int("depth", 5, "how deep to look into subdirectories, 0 means only look at root directory, -1 means no limit")
+	var optFlagFile = flag.String("flag-file", "", "name/glob of the flag file")
+	var optCheckFile = flag.String("check-file", "", "name of the additional file to check")
+	var optCheckRegexp = flag.String("check-regexp", ".*", "regular expression for testing the content of the check file")
+	var optCheckInverse = flag.Bool("check-inverse", false, "regard regular expression not matching as positive")
+	var optExcludes arrayFlag
+	flag.Var(&optExcludes, "exclude", "glob of the directories to exclude")
+	var optNoDefaultExcludes = flag.Bool("no-default-excludes", false, "don't apply default excludes")
+
+	getopt.Aliases(
+		"h", "help",
+		"d", "depth",
+		"f", "flag-file",
+		"c", "check-file",
+		"e", "check-regexp",
+		"i", "check-inverse",
+		"x", "exclude",
+		"n", "no-default-excludes",
+	)
+	getopt.Parse()
+
+	var optRootDir = "."
+	if len(flag.Args()) > 0 {
+		optRootDir = flag.Arg(0)
+	}
+
+	if *optHelp || len(*optFlagFile) == 0 {
+		fmt.Println("Usage: ls-having -f name-or-glob [options] [root-dir]")
+		fmt.Println("Options:")
+		getopt.PrintDefaults()
+		fmt.Println("References:")
+		fmt.Println("  Glob syntax: https://github.com/gobwas/glob#example")
+		fmt.Println("  Regexp syntax: https://pkg.go.dev/regexp/syntax")
+		fmt.Println("  Home page: https://github.com/handy-common-utils/ls-having")
+		if *optHelp {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+
+	if !*optNoDefaultExcludes {
+		optExcludes = append(optExcludes,
 			".git",
 			filepath.Join("**", ".git"),
 			"node_modules",
 			filepath.Join("**", "node_modules"),
-		}, filepath.Separator),
-		FlagFile:     glob.MustCompile(filepath.Join("package.*"), filepath.Separator),
-		CheckFile:    "package.json",
-		CheckRegexp:  regexp.MustCompile("(?m)^ *\"mocha"),
-		CheckInverse: false,
-	}, ".")
+		)
+	}
+
+	var dirs = lsh.LsHaving(&lsh.Options{
+		Depth:        *optDepth,
+		Excludes:     compileGlobs(optExcludes, filepath.Separator),
+		FlagFile:     glob.MustCompile(filepath.Join(*optFlagFile), filepath.Separator),
+		CheckFile:    *optCheckFile,
+		CheckRegexp:  regexp.MustCompile(*optCheckRegexp),
+		CheckInverse: *optCheckInverse,
+	}, optRootDir)
 	fmt.Println(dirs)
 }
 
@@ -32,4 +82,14 @@ func compileGlobs(globStrings []string, separator rune) []glob.Glob {
 		result[i] = glob.MustCompile(globString, separator)
 	}
 	return result
+}
+
+type arrayFlag []string
+
+func (i *arrayFlag) String() string {
+	return strings.Join(*i, ",")
+}
+func (i *arrayFlag) Set(value string) error {
+	*i = append(*i, value)
+	return nil
 }
